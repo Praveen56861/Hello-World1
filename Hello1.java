ff787d795430dc7d5b6fc2eb488ca4a18bd96fcd
Praveen56861 <43000640+Praveen56861@users.noreply.github.com>
2018-09-10 12:12:11 +0530

    Update Hello1.java

--- Hello1.java
+++ Hello1.java
@@ -1,5 +1,5 @@
 class Hello{
     public static void main(String[] args) {
-        System.out.println("Hello, World! and welcome to Devops"); 
+        System.out.println("Hello, World! and welcome to Devops world"); 
     }
 }
